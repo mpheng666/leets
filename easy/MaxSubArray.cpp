79a46d5bd6bc1222d7b08db6f9d88e1888a6fa88
mpheng666 <mpheng96@gmail.com>
2022-05-18 11:39:37 +0800

    MaxSubArray div n conq

--- easy/MaxSubArray.cpp
+++ easy/MaxSubArray.cpp
@@ -1,4 +1,4 @@
-// BRUTE FORCE
+// BRUTE FORCE, N^2
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -35,4 +35,76 @@ public:
     }
 };
 
-// DIVIDE AND CONQUOR
+// LINEAR, N
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum_current = 0;
+        int sum_max = INT_MIN;
+        
+        for(int i=0; i<nums.size(); ++i)
+        {
+            sum_current = sum_current + nums[i];
+            
+            if(sum_current > sum_max)
+            {
+                sum_max = sum_current;
+            }
+            
+            if(sum_current < 0)
+            {
+                sum_current = 0;
+            }
+        }
+        
+        return sum_max;
+    }
+};
+
+// DIVIDE AND CONQUOR, N*LOG(N)
+class Solution {
+public:
+    int max(int x, int y) {return x > y ? x : y;}
+    
+    int maxSubArray_sub(vector<int>& nums, int left, int right)
+    {
+        
+        // for case nums.size() = 0 or 1
+        if(left >= right) {return nums[left];}
+
+        int mid = (left+right)/2;
+        int sum = 0;
+        int max_left = INT_MIN/2;
+        int max_right = INT_MIN/2;
+
+        for(int i=mid; i>=left; --i)
+        {
+            sum +=nums[i];
+            if(sum > max_left)
+            {
+                max_left = sum;
+            }
+        }
+
+        sum = 0;
+        for(int i=mid+1; i<=right; ++i)
+        {
+            sum += nums[i];
+            if(sum > max_right)
+            {
+                max_right = sum;
+            }
+        }
+
+        int left_right_max = max(maxSubArray_sub(nums, left, mid),
+                                 maxSubArray_sub(nums, mid+1, right));
+
+        return max(left_right_max, max_left + max_right);
+    }
+    
+    int maxSubArray(vector<int>& nums) 
+    {
+        return maxSubArray_sub(nums, 0, nums.size()-1);
+    }
+};
