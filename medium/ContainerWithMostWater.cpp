f7c2e54f08eeaf53dad7dee17b19a2e8dd664e54
mpheng666 <mpheng96@gmail.com>
2022-06-27 08:25:09 +0800

    add ContainerWithMostWater idx shifting

--- medium/ContainerWithMostWater.cpp
+++ medium/ContainerWithMostWater.cpp
@@ -22,3 +22,34 @@ public:
         return max_area;
     }
 };
+
+// Index shifting position 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int size = height.size();
+        int left_idx = 0;
+        int right_idx = size-1;
+        int area_max = 0;
+        
+        while(left_idx < right_idx)
+        {
+        //     std::cout << "l: " << left_idx << " r: " << right_idx << std::endl;
+        //     std::cout << "hl: " << height[left_idx] << " hr: " << height[right_idx] << std::endl;
+            area_max = max(area_max, (min(height[left_idx], height[right_idx]) * (right_idx - left_idx)));
+            
+            // std::cout << "area max: " << area_max << std::endl;
+            
+            if(height[left_idx] > height[right_idx])
+            {
+               right_idx--;
+            }
+           else
+           {
+                left_idx++;
+           }
+        }
+        
+        return area_max;
+    }
+};
